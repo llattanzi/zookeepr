70f83965f21df9b4b654c42b553d2ab3fce23a0d
llattanzi <lauren71442@gmail.com>
2022-04-19 18:51:05 -0400

    add static html pages to server and handle GET/POST requests in front-end

--- server.js
+++ server.js
@@ -4,10 +4,13 @@ const path = require('path');
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
+
 // parse incoming string or array data
 app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
 app.use(express.json());
+app.use(express.static('public'));
+
 const { animals } = require('./data/animals');
 
 function filterByQuery(query, animalsArray) {
@@ -118,7 +121,19 @@ app.post('/api/animals', (req, res) => {
 });
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(_dirname, './public/index.html'));
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/animals', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/animals.html'));
+});
+
+app.get('/zookeepers', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
 app.listen(PORT, () => {
